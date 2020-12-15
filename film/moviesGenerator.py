432e16805e288134e819c212fd9deb50373b6dbb
almulalic <almir.mulalic.am@gmail.com>
2020-12-15 21:55:05 +0100

    added random generation for movies tickets invoices and orders

---
+++ film/moviesGenerator.py
@@ -0,0 +1,119 @@
+import urllib.request
+import json
+import random
+import os
+
+dates = [
+    ['2019-01-07','2019-01-14'],
+    ['2019-01-14','2019-01-21'],
+    ['2019-01-21','2019-01-28'],
+    ['2019-01-28','2019-02-04'],
+    ['2019-02-04','2019-02-11'],
+    ['2019-02-11','2019-02-18'],
+    ['2019-02-18','2019-02-25'],
+    ['2019-02-25','2019-03-04'],
+    ['2019-03-04','2019-03-11'],
+    ['2019-03-11','2019-03-18'],
+    ['2019-03-18','2019-03-25'],
+]
+
+jsonFile = open(os.path.join("film/","movies.json"),"w")
+sqlFile = open(os.path.join("film/","movies.sql"),"w")
+
+final = []
+justInserts = []
+
+def normalizeGenre(genreName):
+    if(genreName == 'Documentary'):
+        return 1
+    elif(genreName == 'Drama'):
+        return 2
+    elif(genreName == 'History'):
+        return 3
+    elif(genreName == 'Animation'):
+        return 4
+    elif(genreName == 'Comedy'):
+        return 5
+    elif(genreName == 'Music'):
+        return 6
+    elif(genreName == 'Science Fiction'):
+        return 7
+    elif(genreName == 'Horror'):
+        return 8
+    elif(genreName == 'Mystery'):
+        return 9
+    elif(genreName == 'War'):
+        return 10
+    elif(genreName == 'Action'):
+        return 11
+    elif(genreName == 'Family'):
+        return 12
+    elif(genreName == 'Fantasy'):
+        return 13
+    elif(genreName == 'Adventure'):
+        return 14
+    elif(genreName == 'Romance'):
+        return 15
+    elif(genreName == 'Crime'):
+        return 16
+    elif(genreName == 'Thriller'):
+        return 17
+
+for date in dates:
+    fetch = json.loads(urllib.request.urlopen("https://api.themoviedb.org/3/discover/movie//?api_key=ef57f4b4474cd9d6bd10f63b55c2bac9&primary_release_date.gte=" + date[0] + "&primary_release_date.lte=" + date[1] + "&sort_by=popularity.desc").read())['results']
+
+    sqlList = []
+    count = 1
+
+    for movie in fetch:
+        details = json.loads(urllib.request.urlopen("https://api.themoviedb.org/3/movie/" + str(movie['id']) + "?api_key=ef57f4b4474cd9d6bd10f63b55c2bac9").read())
+        
+        sql = "INSERT INTO dbo.film (naziv,zanrId,producent,datumIzdavanja,trajanjeUMinutama,dobnoOgranicenjeId,je3D,jeSinhronizovan,jeTitlovan,opis) VALUES ("
+
+        title = details['title'].replace("'","''")
+        desc =  details['overview'].replace("'","''")
+
+        values = [
+            "'" + title + "'",
+            normalizeGenre(details['genres'][0]['name']), 
+            "null" if len(details['production_companies']) == 0 else "'" + details['production_companies'][0]['name'] + "'", 
+            "'" + details['release_date'] + "'",
+            details['runtime'],
+            1 if details['adult'] else 2 if random.randint(0,10) % 5 == 0 else 3,
+            0 if random.randint(0,100) % 20 == 0 else 1, 
+            0 if random.randint(0,100) % 40 == 0 else 1, 
+            1,
+            "'" + desc + "'"
+        ]
+
+        
+        for i in range(len(values)):
+            sql += str(values[i]) 
+
+            if(i < len(values) - 1):
+                sql+= ","
+        
+        sql+=");"
+        
+        if(count == 5):
+            break
+        
+        count += 1
+        sqlList.append(sql)
+        justInserts.append(sql)
+
+    red = dict(dateInterval = date[0] + " " + date[1],movies = sqlList)
+    final.append(red)
+
+arr = dict(data=final)
+jsonFile.write(json.dumps(arr))
+
+finalSql = ""
+for insert in justInserts:
+    finalSql += insert + "\n"
+
+sqlFile.write(finalSql)
+
+jsonFile.close()
+sqlFile.close()
+
---
+++ karta/ticketGenerator.py
@@ -0,0 +1,99 @@
+import datetime
+import os
+import random
+import string
+
+sqlFile = open(os.path.join("karta/","kartaNarudzbaRacun.sql"),"w")
+
+brojSedmica = 11
+projekcijaPoDanu = 5
+projekcijaPoFilmu = 5
+danaUSedmici = 5
+brojSala = 5
+
+vrijemePrikazivanja = [11,13,15,18,20]
+kapacitetiSala = [33,10,10,13,11]
+
+
+cijeneNarudzba =  [3,4,5,2,2.5,3,3,3,2.5,3,3,2,3,3,2,2.5,3,3,2,2.5,3,3,4.5,5.5,6.5,5,6,7,5,3,5,3,5,3,2.5,3.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,2.5,4.5,3.5,2.5,3.5,2.5,3.5]
+
+finalniSql = ""
+datum = datetime.datetime(2019,1,7)
+
+globalKarta = 1
+globalNarudzba = 1
+globalRacun = 1
+
+for film in range(1,44):
+    for dan in range(projekcijaPoDanu):
+        for projekcija in range(projekcijaPoFilmu):
+            finalniSql += "/* Projekcija " + str(dan) + " Karte Za Dan " + datum.strftime("%Y-%m-%d") + " */" + "\n"
+            kupljenoKarti = random.randint(kapacitetiSala[film % 5] - 10, kapacitetiSala[film % 5])
+            sjedisteId = 1
+            sjedistaSlova = string.ascii_letters
+            redSlovo = 0
+            for karta in range(kupljenoKarti):
+             
+                kartaSql = "INSERT INTO dbo.karta(filmId,cijena,jeIskoristena,sala,vrijemePrikazivanja,sjediste) VALUES ("
+                
+                values = [
+                    film,
+                    7,
+                    0 if random.randint(0,200) == 54 else 1,
+                    str(projekcija + 1),
+                    "'" + datetime.datetime(datum.year,datum.month,datum.day,vrijemePrikazivanja[film % 5],0,0).strftime("%Y-%m-%d %H:%M:%S") + "'",
+                    "'" + random.choice(sjedistaSlova[redSlovo]).upper() + str(sjedisteId) + "'"
+                ]
+
+                for i in range(len(values)):
+                    kartaSql += str(values[i]);
+                    if(i < len(values) - 1):
+                        kartaSql += ", "
+
+                
+                kartaSql += ");"
+                finalniSql += kartaSql + "\n"
+
+                racunSql = "INSERT INTO dbo.racun (kinoId,kartaId,datum,tipPlacanjaId) VALUES ("
+                racunSql += "1," + str(globalKarta) + ","
+                racunSql += "'" + datetime.datetime(datum.year,datum.month,datum.day,vrijemePrikazivanja[film % 5],0,0).strftime("%Y-%m-%d %H:%M:%S") + "'"
+                racunSql += str(1 if random.randint(0,22) <= 11 else random.randint(2,11))
+                racunSql += ");"                
+
+                finalniSql += racunSql + "\n"
+
+                updateKarte = "UPDATE dbo.racun SET kartaId = " + str(globalKarta) + " WHERE id = " + str(globalRacun) + ";"
+
+                finalniSql += updateKarte + "\n"
+                
+                randNarudzbi = random.randint(0,5)
+                brojNarudzbi = 1 if randNarudzbi <= 3 else 2 if randNarudzbi == 4 else 5
+        
+                for i in range(brojNarudzbi):
+                    narudzbaSql = "INSERT INTO dbo.narudzba (narudzbaTipId,racunId,datum) VALUES ("
+                    narudzbaSql += str(random.randint(1,50)) + ", "
+                    narudzbaSql += str(globalRacun) + ", "
+                    narudzbaSql += "'" + datetime.datetime(datum.year,datum.month,datum.day,vrijemePrikazivanja[film % 5],0,0).strftime("%Y-%m-%d %H:%M:%S") + "'"
+                    narudzbaSql += ");"
+                    finalniSql += narudzbaSql + "\n"
+                    globalNarudzba += 1
+
+
+                globalKarta+=1
+                globalRacun+=1
+                sjedisteId+=1
+
+                if(sjedisteId > 5):
+                    redSlovo += 1
+                
+        finalniSql += "/* Kraj dana \n */"
+        datum = datum + datetime.timedelta(days=1)
+
+        if(datum.weekday() == 5):
+            datum = datum + datetime.timedelta(days=2)
+
+    finalniSql += "/* Kraj filma \n */"
+
+sqlFile.write(finalniSql)
+
+sqlFile.close()
